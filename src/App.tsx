def1ee8e6f6858fcf27faa86caacdd5afb26890a
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-08-10 14:31:22 +0300

    Bug Tracker
    * Creating the form.

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,28 @@ function App() {
 
   const addBug = () => {}
   const deleteBug = () => {}
-  
+
   return (
     <div className="App">
       <h1>🐞 Bug Tracker</h1>
+
+      <form onSubmit={addBug}>
+        <label htmlFor="newBugDescription">
+          New bug description: 
+        </label>
+        <input type="text" id="newBugDescription" value={newBugDescription} onChange={e => setNewBugDescription(e.target.value)} />
+        
+        <label htmlFor="newBugPriority">
+          New bug priority:
+        </label>
+        <select id="newBugPriority" value={newBugPriority} onChange={e => setNewBugPriority(e.target.value)}>
+          <option value="Low"> Low </option>
+          <option value="Medium"> Medium </option>
+          <option value="High"> High </option>
+        </select>
+
+        <button type="submit"> Add New Bug </button>
+      </form>
     </div>
   );
 }
